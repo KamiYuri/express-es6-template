6e04ace82ccacc335229614b6e2fdd59e6ec9429
Viet Dung Hoang <dung.hv194254@sis.hust.edu.vn>
2022-12-01 19:11:12 +0700

    add sequelize

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
-import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import passport from "passport";
+import LocalStrategy from "passport-local";
 
-import indexRouter from './routes/index.js';
-import usersRouter from './routes/users.js';
+//Import router
+import indexRouter from "./routes/index.js";
+import usersRouter from "./routes/users.js";
+import db from "./database/models/index.js";
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//Using router
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// db.sequelize.sync({ alter: true });
 
 export default app;
